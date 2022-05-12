7eb3fe2dba1cf053fff94e8565462f60b2a8eda0
Jordan Dickerson <jordanmatthewdickerson@gmail.com>
2022-05-11 23:39:34 -0400

    working on the buttons

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
+import { render } from '@testing-library/react';
 import './App.css';
 
 function App() {
 
-  return (
-    <div className="App">
-      <div className="App-Calculator">
-        <button>C</button>
-        <button>()</button>
-        <button>%</button>
-        <button>/</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>X</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>+</button>
-        <button>+/-</button>
-        <button>0</button>
-        <button>.</button>
-        <button>=</button>
+  function Row(button1, button2, button3, button4, button5, button6) {
+    this.button1 = button1;
+    this.button2 = button2;
+    this.button3 = button3;
+    this.button4 = button4;
+    this.button5 = button5;
+    this.button6 = button6;
+
+    this.row = (button1, button2, button3, button4, button5, button6) => {
+      let row = "";
+      for (let x in Row)
+        row += "<button>"+ [x] +"</button>";
+      return row;
+    }
+  };
+
+  const row1 = new Row ("RAD | Deg", "x!", "(", ")", "%", "AC");
+  const row2 = new Row ("lnv", "sin", "7", "8", "9", "%");
+  const row3 = new Row ("ln", "cos", "4", "5", "6", "x");
+  const row4 = new Row ("e", "tan", "1", "2", "3", "-");
+  const row5 = new Row ("Ans", "EXP", "0", ".", "=", "+");
+
+
+  render () {
+
+    
+    return (
+      <div className="App">
+        <div className="App-Calculator">
+        
+        
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default App;

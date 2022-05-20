23d3dab47db1cf46bcc82c26c43daeb29afedef5
Jordan Dickerson <jordanmatthewdickerson@gmail.com>
2022-05-20 17:31:20 -0400

    almost done with pemdas

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import './App.css';
 
 function App() {
 
-  function Row(button1, button2, button3, button4, button5, button6) {
+  function Row(button1, button2, button3, button4) {
     this.button1 = <button onClick={() => calculate(button1)}>{button1}</button>;
     this.button2 = <button onClick={() => calculate(button2)}>{button2}</button>;
     this.button3 = <button onClick={() => calculate(button3)}>{button3}</button>;
     this.button4 = <button onClick={() => calculate(button4)}>{button4}</button>;
-    this.button5 = <button onClick={() => calculate(button5)}>{button5}</button>;
-    this.button6 = <button onClick={() => calculate(button6)}>{button6}</button>;
   };
 
-  const row1 = new Row ("abs", "pow", "(", ")", "%", "CE");
-  const row2 = new Row ("sqrt", "sin", "7", "8", "9", "/");
-  const row3 = new Row ("log", "cos", "4", "5", "6", "X");
-  const row4 = new Row ("PI", "tan", "1", "2", "3", "-");
-  const row5 = new Row ("Ans", "exp", "0", ".", "=", "+");
+  
+  const row1 = new Row ("7", "8", "9", "/");
+  const row2 = new Row ("4", "5", "6", "x");
+  const row3 = new Row ("1", "2", "3", "-");
+  const row4 = new Row ("0", ".", "=", "+");
 
   let calculator = [];
   let total = 0, answer = 0, answer2 = 0; 
@@ -33,7 +31,7 @@ function App() {
       case "/":
         total = answer / answer2;
       break;
-      case "X":
+      case "x":
         total = answer * answer2;
       break;
       case "%":
@@ -45,23 +43,34 @@ function App() {
   }
 
   function findOperator(calculator, char) {
-      if(char === "("){
-        for (let x of calculator) {
-          let start = 0, end = 0, returnArray = [];
-          let text = calculator.join("").toString();
-          start = text.search("(");
-          end = text.search(")");
-          start += 1;
-          return returnArray = calculator.slice(start,end);
+    let returnArray = [], loopArray = calculator;
+    let start = 0, end = 0;
+    for(let x = 0; x < loopArray.length; x++) {
+      if(loopArray[x] === char){
+        start = loopArray.indexOf(loopArray[x]);
+        end = start + 2;
+        if(end > loopArray.length){
+          end = start + 1;
         }
+        start -= 1;
+        while(loopArray[end] === char){
+          end += 2;
+        }
+        returnArray.push(loopArray.splice(start,end));
       }
-      
+    }
+    returnArray.reverse();
+    return returnArray;
   }
-
+  
   function pemdas(calculator){
-    let p=[], e=[], m=[], d=[], a=[], s=[];
-    p = findOperator(calculator,"(");
-    
+    let m=[], d=[], a=[], s=[];
+    m = findOperator(calculator, "x");
+    d = findOperator(calculator, "/");
+    a = findOperator(calculator, "+");
+    s = findOperator(calculator, "-");
+    calculator = m.concat(d,a,s);
+    return calculator;
   }
   
   function calculate(info) {
@@ -71,11 +80,11 @@ function App() {
     if(info === "="){
       calculator.pop();
 
+      calculator = pemdas(calculator);
       
-
       for (let x of calculator) {
         let counter = 0;
-        if(x === "-" || x === "+" || x === "/" || x === "X" || x === "%" || x === "."){
+        if(x === "-" || x === "+" || x === "/" || x === "x" || x === "%" || x === "."){
           for(let x of numberArray){
             number += x;
           }
@@ -113,11 +122,10 @@ function App() {
       <div className="App">
         <div id='App-Calculator'></div>
         <div className="App-Buttons">
-          {row1.button1}{row1.button2}{row1.button3}{row1.button4}{row1.button5}{row1.button6}
-          {row2.button1}{row2.button2}{row2.button3}{row2.button4}{row2.button5}{row2.button6}
-          {row3.button1}{row3.button2}{row3.button3}{row3.button4}{row3.button5}{row3.button6}
-          {row4.button1}{row4.button2}{row4.button3}{row4.button4}{row4.button5}{row4.button6}
-          {row5.button1}{row5.button2}{row5.button3}{row5.button4}{row5.button5}{row5.button6}
+          {row1.button1}{row1.button2}{row1.button3}{row1.button4}
+          {row2.button1}{row2.button2}{row2.button3}{row2.button4}
+          {row3.button1}{row3.button2}{row3.button3}{row3.button4}
+          {row4.button1}{row4.button2}{row4.button3}{row4.button4}
         </div>
       </div>
     );
